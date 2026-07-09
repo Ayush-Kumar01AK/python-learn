c5f1305f0813cfce1045824ee87f022601cc146a
Ayush Kumar <ayushmeghpurala2006@gmail.com>
2026-07-09 16:14:01 +0530

    chapter 7

---
+++ chapter7/05_for_loop_iterate.py
@@ -0,0 +1,14 @@
+# 'for' loop with list
+l = [1,4,6,234,6,764]
+for i in l:
+    print(i)
+
+#'for' loop with tuples
+t = (6,231,75,122)
+for i in t:
+    print(i)
+
+#'for' loop with strigs
+s = "wisdom"
+for i in s:
+    print(i)    
