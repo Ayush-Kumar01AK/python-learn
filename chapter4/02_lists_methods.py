e5fb719e30584c81e1ef78487f93c0919a694c46
Ayush Kumar <ayushmeghpurala2006@gmail.com>
2026-07-09 16:10:45 +0530

    chapter 4

---
+++ chapter4/02_lists_methods.py
@@ -0,0 +1,19 @@
+friends = ["Apple","Orange",5,345.06,False,"Rohan"]
+# print(friends[0])
+friends.append("Ayush")
+print(friends)
+
+l = [1,34,62,2,6,11]
+l.sort()
+print(l)
+
+l.reverse()
+print(l)
+l.insert(3,333)
+print(l)
+
+l.pop(3)
+print(l)
+
+print(l.pop(3)) # it print pop number
+print(l)   # it prients list without pop number
---
+++ chapter4/03_tuple.py
@@ -0,0 +1,9 @@
+a = (1,2,5,6)
+print(type(a))
+
+a = (1) # it give type int
+print(type(a))
+
+# for one number as tuple type comma ,
+a = (1,)
+print(type(a))
