86f698117502ba37e901f50315850b81f063dddd
Ayush Kumar <ayushmeghpurala2006@gmail.com>
2026-07-09 16:12:48 +0530

    chapter 6

---
+++ chapter6/02_multiple_if_statment.py
@@ -0,0 +1,15 @@
+a = int(input("entre your age: "))
+
+if(a%2==0):
+    print("age is even")
+
+if(a>=18):
+    print("your are eligible")
+elif(a<0):
+    print("entre valid age")
+elif(a==0):
+    print("ye anda apne ghar le jao")
+else:
+    print("your are not eligible") 
+
+print("Thanks")       
